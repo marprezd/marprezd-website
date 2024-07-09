c78489d7278e79bd754d6d9ce95e56b6864ecf42
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2024-07-09 15:41:25 -0500

    Add Embla carousel.

---
+++ components/carousel/embla-carousel.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { EmblaOptionsType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+import { useTranslations } from 'next-intl'
+import React from 'react'
+import { Post } from '@/.velite'
+import PostCard from '../posts/post-card'
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons,
+} from './embla-carousel-arrow-buttons'
+
+type PropType = {
+  posts: Post[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const t = useTranslations('site')
+  const { options, posts } = props
+  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi)
+
+  return (
+    <section className="embla">
+      <div className="mb-4 flex flex-row items-center justify-between">
+        <h2 className="text-lg text-neutral-800 dark:text-neutral-200">{t('posts.similar_posts.title')}</h2>
+        <div className="embla__controls">
+          <div className="embla__buttons">
+            <PrevButton disabled={prevBtnDisabled} onClick={onPrevButtonClick} />
+            <NextButton disabled={nextBtnDisabled} onClick={onNextButtonClick} />
+          </div>
+        </div>
+      </div>
+      <div ref={emblaRef} className="embla__viewport">
+        <div className="embla__container">
+          {posts.slice(0, 6).map(post => (
+            <div key={post.slug} className="embla__slide">
+              <PostCard post={post} shadow="none" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default EmblaCarousel
