3ab5bf55351ad21cda4ee27528805cf8037abfe8
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2024-07-10 10:46:57 -0500

    Update Wakapi types.

--- types/index.ts
+++ types/index.ts
@@ -6,63 +6,25 @@ export type IconSvgProps = SVGProps<SVGSVGElement> & {
 
 export type WakapiSummaries = {
   data: {
-    data: [
-      {
-        categories: [
-          {
-            name: string
-            total_seconds: number
-            text: string
-          }
-        ]
-        languages: [
-          {
-            name: string
-            total_seconds: number
-          },
-        ]
-        grand_total: {
-          minutes: number
-          text: string
-        }
-        range: {
-          date: string
-          timezone: string
-        }
-      },
-    ],
     cumulative_total: {
-      seconds: number
       text: string
+      seconds: number
     }
     daily_average: {
-      days_including_holidays: number
       days_minus_holidays: number
       holidays: number
       seconds: number
     }
+    end: string
   }
 }
 
 export type WakapiStats = {
   data: {
     data: {
-      total_seconds: number
-      daily_average: number
-      human_readable_range: string
-      human_readable_total: string
-      human_readable_daily_average: string
-      editors: [
-        {
-          name: string
-          text: string
-          total_seconds: number
-        },
-      ]
       languages: [
         {
           name: string
-          text: string
           total_seconds: number
         },
       ]
