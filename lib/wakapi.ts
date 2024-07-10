5fa3900775a0422db80c1a83703016a45f43d8eb
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2024-07-10 18:36:44 -0500

    Update Wakapi endpoint.

--- lib/wakapi.ts
+++ lib/wakapi.ts
@@ -4,7 +4,8 @@ const wakapi_user: string = process.env.WAKAPI_USER || ''
 const WAKAPI_ENDPOINT_LAST_14_DAYS = `https://wakapi.dev/api/compat/wakatime/v1/users/${wakapi_user}/stats/last_14_days`
 const WAKAPI_ENDPOINT_LAST_30_DAYS = `https://wakapi.dev/api/compat/wakatime/v1/users/${wakapi_user}/stats/last_30_days`
 const WAKAPI_ENDPOINT_SUMMARIES_LAST_7_DAYS = `https://wakapi.dev/api/compat/wakatime/v1/users/${wakapi_user}/summaries?range=last_7_days`
-const WAKAPI_ENDPOINT_SUMMARIES_YESTERDAY = `https://wakapi.dev/api/compat/wakatime/v1/users/current/summaries?range=yesterday`
+const WAKAPI_ENDPOINT_SUMMARIES_YESTERDAY = `https://wakapi.dev/api/compat/wakatime/v1/users/${wakapi_user}/summaries?range=yesterday`
+const WAKAPI_ENDPOINT_SUMMARIES_TODAY = `https://wakapi.dev/api/compat/wakatime/v1/users/${wakapi_user}/summaries?range=today`
 
 export const getLast14DaysStats = async () => {
   return fetch(WAKAPI_ENDPOINT_LAST_14_DAYS, {
@@ -38,7 +39,17 @@ export const getSummariesLast7DaysStats = async () => {
 
 export const getSummariesYesterday = async () => {
   return fetch(WAKAPI_ENDPOINT_SUMMARIES_YESTERDAY, {
-    next: { revalidate: 60 * 60 * 12 },
+    next: { revalidate: 60 * 60 * 24 },
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${Buffer.from(wakapi_api_key).toString('base64')}`,
+    },
+  })
+}
+
+export const getSummariesToday = async () => {
+  return fetch(WAKAPI_ENDPOINT_SUMMARIES_TODAY, {
+    next: { revalidate: 60 * 60 * 2 },
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Basic ${Buffer.from(wakapi_api_key).toString('base64')}`,
