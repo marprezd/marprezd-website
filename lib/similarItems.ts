04e4938d70773071f8709257530ffa9f881b6958
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2024-07-11 19:19:03 -0500

    Add filter functionality to find similar posts.

---
+++ lib/similarItems.ts
@@ -0,0 +1,41 @@
+import { Post } from '@/.velite'
+
+// similer products
+const similerItems = (
+  currentItem: Post,
+  allItems: Post[],
+  slug: string,
+): Post[] => {
+  let tags: string[] = []
+  let categories: string[] = []
+
+  // set categories
+  if (currentItem.categories.length > 0) {
+    categories = currentItem.categories
+  }
+
+  // set tags
+  if (currentItem.tags.length > 0) {
+    tags = currentItem.tags
+  }
+
+  // filter by categories
+  const filterByCategories = allItems.filter((item: any) =>
+    categories.find((category) => item.categories.includes(category))
+  )
+
+  // filter by tags
+  const filterByTags = allItems.filter((item: any) =>
+    tags.find(tag => item.tags.includes(tag)),
+  )
+
+  // merged after filter
+  const mergedItems = [...new Set([...filterByCategories,...filterByTags])]
+
+  // filter by slug
+  const filterBySlug = mergedItems.filter(product => product.slug !== slug)
+
+  return filterBySlug
+}
+
+export default similerItems
